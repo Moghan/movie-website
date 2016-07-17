2c1add13fd22ea0083e73ac9d449fe654d9da9fc
Moghan <anders.kensby@gmail.com>
2016-07-17 21:01:42 +0200

    doc:	Add comments

--- movie.py
+++ movie.py
@@ -1,5 +1,7 @@
 class Movie():
+    ''' Class that describes a movie '''
     def __init__(self, mTitle, mTrailer, mImage):
+        ''' Constructor that sets instance variables using in parameters '''
         self.title = mTitle
         self.trailer_youtube_url = mTrailer
         self.poster_image_url = mImage
--- movie_website.py
+++ movie_website.py
@@ -2,10 +2,13 @@ import fresh_tomatoes
 import movie
 import json
 
+# List for storing movies
 movies = []
 
+# Open file and append movies found to the movies list
 with open('movies.json') as data_file:    
     for mData in json.load(data_file):
         movies.append(movie.Movie(mData['title'], mData['trailer'], mData['image']))
 
+# external website generator with list of movies as parameter
 fresh_tomatoes.open_movies_page(movies)
